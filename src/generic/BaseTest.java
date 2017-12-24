a5a95ca8f2aa0cfc7282ffc802d03544355552a5
Anjali598 <34765826+Anjali598@users.noreply.github.com>
2017-12-24 13:20:36 +0530

    commit

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
+//import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public abstract class BaseTest implements IAutoConst {
@@ -20,11 +20,11 @@ public abstract class BaseTest implements IAutoConst {
 	}
     @Parameters({"browser"})
 	@BeforeMethod(alwaysRun=true)
-	public void openApplication(@Optional("chrome") String browser) {
+	public void openApplication(String browser) {
 		if(browser.equals("chrome")) {
-    	driver =new ChromeDriver();
+			driver=new ChromeDriver();
 		}else {
-			driver =new FirefoxDriver();
+			driver=new FirefoxDriver();
 		}
 		
 		String AUT = AUL.getProperty(SETTING_PATH, "AUT");
--- src/generic/IAutoConst.java
+++ src/generic/IAutoConst.java
@@ -2,10 +2,10 @@ package generic;
 
 public interface IAutoConst {
 
-	String CHROME_KEY = "webdeiver.chrome.driver";
-	String CHROME_VALUE ="./Data/chromedriver.exe";
-	String GECKO_KEY = "webdeiver.gecko.driver";
-	String GECKO_VALUE ="./Data/geckodriver.exe";
+	String CHROME_KEY = "webdriver.chrome.driver";
+	String CHROME_VALUE ="./Driver/chromedriver.exe";
+	String GECKO_KEY = "webdriver.gecko.driver";
+	String GECKO_VALUE ="./Driver/geckodriver.exe";
 	String XL_PATH ="./Data/input.xlsx";
 	String SETTING_PATH="settings.properties";
 	String PHOTO_PATH="./photo/";
